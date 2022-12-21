3e42085f19589beeaa656e445f184c5408931f78
BloOdx16 <121193958+BloOdx16@users.noreply.github.com>
2022-12-22 00:28:05 +0530

    Add files via upload

---
+++ tutorial.js
@@ -0,0 +1,7 @@
+console.log("LOGIN INFO");
+let _username=document.getElementById("usn");
+let _password=document.getElementById("psw");
+document.getElementById("btn").addEventListener('click',function(){
+    console.log(_username.value);
+    console.log(_password.value);
+})
